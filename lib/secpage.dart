170926054ceb7f7deea98adac2ecc23c50f3c56c
thomasjoseph22 <thomasjoseph1243@gmail.com>
2022-12-05 18:36:48 +0530

    cyberhawk

--- lib/secpage.dart
+++ lib/secpage.dart
@@ -48,7 +48,7 @@ class _SecpageState extends State<Secpage> {
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: Container(
-                height: 100,
+                height: 200,
                 width: 400,
                 decoration: BoxDecoration(
                   border: Border.all(
@@ -58,7 +58,7 @@ class _SecpageState extends State<Secpage> {
                   borderRadius: BorderRadius.circular(8),
                   boxShadow: const [
                     BoxShadow(
-                      color: Color.fromARGB(255, 31, 36, 31),
+                      color: Color.fromARGB(255, 0, 0, 0),
                       blurRadius: 10,
                       spreadRadius: 5,
                     ),
@@ -86,7 +86,7 @@ class _SecpageState extends State<Secpage> {
                             padding: const EdgeInsets.only(top: 7),
                             child: SizedBox(
                               child: ToggleSwitch(
-                                customWidths: const [80.0, 50.0],
+                                customWidths: const [70.0, 40.0],
                                 cornerRadius: 20.0,
                                 activeBgColors: const [
                                   [Color.fromARGB(255, 47, 161, 19)],
@@ -102,6 +102,137 @@ class _SecpageState extends State<Secpage> {
                             ),
                           ),
                         ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              'Is there any Suspect',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'IBM',
+                                  fontSize: 15),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 7),
+                            child: SizedBox(
+                              child: ToggleSwitch(
+                                customWidths: const [70.0, 40.0],
+                                cornerRadius: 20.0,
+                                activeBgColors: const [
+                                  [Color.fromARGB(255, 152, 160, 150)],
+                                ],
+                                activeFgColor: Colors.white,
+                                inactiveBgColor: Colors.grey,
+                                inactiveFgColor: Colors.white,
+                                totalSwitches: 1,
+                                labels: const [
+                                  'NO',
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              'Is there any Suspect',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'IBM',
+                                  fontSize: 15),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 7),
+                            child: SizedBox(
+                              child: ToggleSwitch(
+                                customWidths: const [70.0, 40.0],
+                                cornerRadius: 20.0,
+                                activeBgColors: const [
+                                  [Color.fromARGB(255, 152, 160, 150)],
+                                ],
+                                activeFgColor: Colors.white,
+                                inactiveBgColor: Colors.grey,
+                                inactiveFgColor: Colors.white,
+                                totalSwitches: 1,
+                                labels: const [
+                                  'NO',
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                height: 200,
+                width: 380,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text(
+                              'comments',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'IBM',
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus try’s standard dummy text ever since the 1500s,',
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'IBM',
+                            fontSize: 15),
                       ),
                     ),
                   ],
